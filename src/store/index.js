090f0b5372e28af87588bd40d5e72885e9838195
João Pedro <joapedro10782@gmail.com>
2021-06-22 12:42:34 -0300

    console log removal

--- src/store/index.js
+++ src/store/index.js
@@ -41,7 +41,7 @@ const actions = {
         return new Promise((resolve, reject) => {
             http.get()
                 .then(res => {
-                    console.log(res)
+                    // console.log(res)
                     resolve(res.data);
 
                 })
